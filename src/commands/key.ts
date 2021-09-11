01bee94586e92c30d9a52bb9bd81160763a543ae
surajkarki66 <suraj.karki500@protonmail.com>
2021-09-11 14:25:44 +0545

    Created isRequired validation utility function

--- src/commands/key.ts
+++ src/commands/key.ts
@@ -1,6 +1,24 @@
+import inquirer from "inquirer";
+import colors from "colors";
+
+import validation from "../utils/validation";
+import KeyManager from "../lib/KeyManager";
+
 const key = {
-  set() {
-    console.log("hello from  set");
+  async set() {
+    const keyManager = new KeyManager();
+    const input = await inquirer.prompt([
+      {
+        type: "input",
+        name: "key",
+        message: "Enter API Key".green + "https://nomics.com",
+        validate: validation.isRequired,
+      },
+    ]);
+    const key = keyManager.setKey(input.key);
+    if (key) {
+      console.log("API Key set".blue);
+    }
   },
   show() {
     console.log("hello from show");
---
+++ src/utils/validation.ts
@@ -0,0 +1,5 @@
+// Required fields
+const isRequired = (input: any) =>
+  input === "" ? "This value is required" : true;
+
+export default { isRequired };
