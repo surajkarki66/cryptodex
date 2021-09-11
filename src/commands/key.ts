781ec7af4c77bf0cc1a3262ade46ff89f64be409
surajkarki66 <suraj.karki500@protonmail.com>
2021-09-11 13:53:26 +0545

    Created separate commands directory

---
+++ src/commands/key.ts
@@ -0,0 +1,13 @@
+const key = {
+  set() {
+    console.log("hello from  set");
+  },
+  show() {
+    console.log("hello from show");
+  },
+  remove() {
+    console.log("hello from remove");
+  },
+};
+
+export default key;
--- src/cryptodex-key.ts
+++ src/cryptodex-key.ts
@@ -1,18 +1,14 @@
 import { program } from "commander";
 
+import key from "./commands/key";
+
 program
   .command("set")
   .description("Set API Key -- Get at https://nomics.com")
-  .action(() => console.log("Hello from set"));
+  .action(key.set);
 
-program
-  .command("show")
-  .description("Show API Key")
-  .action(() => console.log("Hello from show"));
+program.command("show").description("Show API Key").action(key.show);
 
-program
-  .command("remove")
-  .description("Remove API Key")
-  .action(() => console.log("Hello from remove"));
+program.command("remove").description("Remove API Key").action(key.remove);
 
 program.parse(process.argv);
